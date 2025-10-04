dd1977b422b76d52ffcdc92cd89bec392c1cfbef
Ankit Chauhan <chauhanankit4591@gmail.com>
2025-10-04 01:44:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -145,5 +145,9 @@ def reset_password(token):
     return render_template('reset_password.html', token=token)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
